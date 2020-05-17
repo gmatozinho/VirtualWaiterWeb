5faa063b2df1b5aee9319fb0190bb662daa9f244
Gmatozinho <gustavomatozinho@gmail.com>
2020-05-17 03:02:07 -0300

    ajust

--- post_install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-cd /home/ec2-user/VirtualWaiterWeb
-npm install
-npm i -g @angular/cli
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /home/ec2-user/VirtualWaiterWeb
-#pm2 start npm --name VirtualWaiter -- start --watch
